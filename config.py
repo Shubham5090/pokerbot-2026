dc8026e8f16c49e29b7bc610ef8c5640ccc149fc
Harsh-Jaju <cs24b076@smail.iitm.ac.in>
2026-03-02 00:04:18 +0530

    new

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BOT_1_NAME = 'bot1'
 BOT_1_FILE = './example_bot.py'
 
 BOT_2_NAME = 'example_bot'
-BOT_2_FILE= './example_bot.py'
+BOT_2_FILE= './bot3'
 
 # GAME PROGRESS IS RECORDED HERE
 GAME_LOG_FOLDER = './logs'
